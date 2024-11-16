c65c420e22e439d0b07a693ed591c10ae7ec76b8
Shivam <beoharshivam@gmail.com>
2024-11-16 05:41:51 +0530

    TwoSum brute force solution

---
+++ src/arrays/TwoSum.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] nonOptimalResult = twoSumNotOptimalSolution(nums, target);
+        System.out.println(Arrays.toString(nonOptimalResult));
+    }
+
+    public static int[] twoSumNotOptimalSolution(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
