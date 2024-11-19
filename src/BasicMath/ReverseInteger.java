b1a09aa2bf5f6f782b653329da9019a7de488156
Shivam <beoharshivam@gmail.com>
2024-11-20 02:18:59 +0530

    Reverse Integer

---
+++ src/BasicMath/ReverseInteger.java
@@ -0,0 +1,18 @@
+package BasicMath;
+
+/**
+ * Reverse an Integer.
+ */
+public class ReverseInteger {
+    public static void main(String[] args) {
+        int num = 123456;
+        int temp = num;
+        int result = 0;
+        while (temp != 0) {
+            int lastDigit = temp % 10;
+            result = result * 10 + lastDigit;
+            temp = temp / 10;
+        }
+        System.out.println("The reverse of " + num + " is " + result);
+    }
+}
