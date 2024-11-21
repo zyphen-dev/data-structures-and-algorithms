b69ddfd73966e316bc45a261538ff66ef2470275
Shivam <beoharshivam@gmail.com>
2024-11-22 03:00:47 +0530

    print names using recursion

--- src/recursion/PrintNames.java
+++ src/recursion/PrintNames.java
@@ -1,4 +1,26 @@
 package recursion;
 
 public class PrintNames {
+    public static void main(String[] args) {
+        printName(1, 10);
+    }
+
+    /**
+     * Recursively prints a sequence of numbers followed by " - Java".
+     * The method will print all integers from 'from' to 'to' inclusive,
+     * each followed by " - Java" on a new line.
+     *
+     * @param from from The starting number (inclusive)
+     * @param to   to The ending number (inclusive)
+     */
+    public static void printName(int from, int to) {
+        //Base case: stop recursion when we've printed all numbers up to 'to'
+        if (from > to) {
+            return;
+        }
+        System.out.println(from + " - Java");
+
+        //Recursive case: call the method again with the next number
+        printName(from + 1, to);
+    }
 }
