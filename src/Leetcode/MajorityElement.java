5fa90b6df3e09ea2d7677e6714f04890444cd33f
zyphen-dev <shivambeohar.dev@gmail.com>
2026-02-22 09:14:23 +0530

    leetcode - easy - majorityElement - BoyerMoore Algorith

--- src/Leetcode/MajorityElement.java
+++ src/Leetcode/MajorityElement.java
@@ -33,7 +33,9 @@ class MajorityElement {
 
     public static void main(String[] args) {
         int[] nums = {2, 2, 1, 1, 1, 2, 2};
+//        int[] nums = {3,2,3};
         System.out.println("The Majority Element for given array is = " + majorityElement(nums));
+        System.out.println("The Majority Element for given array is = " + majorityElementUsingBoyerMooreAlgorithm(nums));
     }
 
     public static int majorityElement(int[] nums) {
@@ -54,5 +56,27 @@ class MajorityElement {
             }
         }
         return 0;
+    }
+
+    public static int majorityElementUsingBoyerMooreAlgorithm(int[] nums) {
+        // Concept of Candidate and Count
+        // Here candidate is array element and count its frequency in the array.
+        // {2, 2, 1, 1, 1, 2, 2}
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (candidate == nums[i]) {
+                count++;
+            } else if (candidate != nums[i]) {
+                count--;
+            }
+
+            if (count == 0) {
+                candidate = nums[i];
+                count++;
+            }
+        }
+        return candidate;
     }
 }
